807ff599cba8957f984ae963df3152d932e0676c
Oguz Konya <oguzkonya34@gmail.com>
2021-03-21 10:25:50 +0000

    Fix eksi links

--- parser.py
+++ parser.py
@@ -79,7 +79,7 @@ def parseEntry(debe, nextDebe, printer):
             entry.date = bs.find("a", {"class": "entry-date"}).text
             entry.author = bs.find("a", {"class": "entry-author"}).text
             entry.authorLink = BIRI + "/" + entry.author.replace(" ", "-")
-            entry.content = bs.find("div", {"class": "content"}).decode_contents().replace('href="/?q=', 'href="' + BASE_URL + '/?q=')
+            entry.content = bs.find("div", {"class": "content"}).decode_contents().replace('href="/?q=', 'href="' + BASE_URL + '/?q=').replace('href="/entry', 'href="' + BASE_URL + '/entry')
 
             return printer.printEntry(entry)
 
